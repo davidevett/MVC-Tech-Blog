8d9d81a12ac09f3d6edf0d361f744a011f3ffab0
Delta <davidevett55@gmail.com>
2024-08-22 18:52:41 -0500

    last commit

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Comment = require('./comment');
-const Post = require('./post');
+const Comment = require('./Comment');
+const Post = require('./Post');
 
 User.hasMany(Comment, { foreignKey: 'user_id' });
 Comment.belongsTo(User, { foreignKey: 'user_id' });
